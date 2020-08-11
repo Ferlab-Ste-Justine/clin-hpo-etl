bf85499c926b5456fa1b0cef05eb775722984695
jecos <jcostanza@ferlab.bio>
2020-08-11 08:55:22 -0400

    Initial commit

---
+++ src/main/scala/bio/ferlab/clin/idx/examples/Updater2.scala
@@ -0,0 +1,36 @@
+package bio.ferlab.clin.idx.examples
+
+import org.apache.spark.sql.SparkSession
+import org.elasticsearch.spark.sql._
+
+case class Data(id: String, person: Person, address: Option[Address])
+
+case class Person(name: String, age: Int)
+
+object Updater extends App {
+
+  implicit val spark: SparkSession = SparkSession.builder
+        .master("local")
+    .config("es.index.auto.create", "true")
+    .appName(s"Indexer").getOrCreate()
+
+  import spark.implicits._
+
+
+  val df = Seq(
+    Data("A", Person("Jean", 12), Some(Address("WA", 500 ))),
+    Data("B",
+      Person("George", 38),
+      Some(Address("NY", 345 ))
+    )
+    ).toDF
+
+
+  df.saveToEs("spark4/docs", Map("es.mapping.id" -> "id"))
+//, "es.write.operation" -> "update"
+
+}
+
+case class Address(city: String, civic: Int)
+
+
